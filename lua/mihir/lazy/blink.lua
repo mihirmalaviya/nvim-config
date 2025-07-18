80422db3a6ba7ec9a8267f387d08cef41ab87a3b
MihirMalaviya <mihirmalaviya1@gmail.com>
2025-07-18 04:47:46 -0400

    added autocomplete

---
+++ lua/mihir/lazy/blink.lua
@@ -0,0 +1,24 @@
+return {
+	"saghen/blink.cmp",
+	dependencies = { "rafamadriz/friendly-snippets" },
+
+	version = "1.*",
+
+	opts = {
+		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+		--
+		-- All presets have the following mappings:
+		-- C-space: Open menu or open docs if already open
+		-- C-n/C-p or Up/Down: Select next/previous item
+		-- C-e: Hide menu
+		-- C-k: Toggle signature help (if signature.enabled = true)
+		keymap = { preset = "default" },
+
+		appearance = {
+			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- Adjusts spacing to ensure icons are aligned
+			nerd_font_variant = "mono",
+		},
+	},
+	opts_extend = { "sources.default" },
+}
