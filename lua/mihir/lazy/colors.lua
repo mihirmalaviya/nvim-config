c305f2e07609c0bdd592c5de70928813c1587512
MihirMalaviya <mihirmalaviya1@gmail.com>
2025-07-18 04:50:34 -0400

    added some themes and a way to switch quickly

--- lua/mihir/lazy/colors.lua
+++ lua/mihir/lazy/colors.lua
@@ -1,49 +1,60 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	lazy = false,
-	config = function()
-		require("gruvbox").setup({
-			terminal_colors = true, -- add neovim terminal colors
-			undercurl = true,
-			underline = false,
-			bold = true,
-			italic = {
-				strings = false,
-				emphasis = false,
-				comments = false,
-				operators = false,
-				folds = false,
-			},
-			strikethrough = true,
-			invert_selection = true,
-			invert_signs = false,
-			invert_tabline = false,
-			invert_intend_guides = false,
-			inverse = true, -- invert background for search, diffs, statuslines and errors
-			contrast = "hard", -- can be "hard", "soft" or empty string
-			-- palette_overrides = {
-			-- 	dark0_hard = "#0c0d0e",
-			-- },
-			overrides = {},
-			dim_inactive = false,
-			transparent_mode = false,
-		})
-		vim.cmd("colorscheme gruvbox")
+	{
+		"zaldih/themery.nvim",
+		lazy = false,
+		config = function()
+			require("themery").setup({
+				themes = {
+					"gruvbox",
+					"gruvbox-material",
+					"kanagawa-wave",
+					"kanagawa-dragon",
+					"rose-pine",
+					"ayu",
+				},
+			})
 
-		-- local colors = require("gruvbox").palette
-		--
-		-- vim.o.termguicolors = true
-		--
-		-- -- vim.api.nvim_set_hl(0, "CursorNormal", { fg = normal.bg, bg = normal.fg })
-		-- vim.api.nvim_set_hl(0, "CursorInsert", { fg = colors.dark0_hard, bg = colors.bright_blue })
-		-- vim.api.nvim_set_hl(0, "CursorVisual", { fg = "#ffffff", bg = "#000000" })
-		-- -- vim.api.nvim_set_hl(0, "CursorReplace", { fg = normal.bg, bg = yellow.fg })
-		--
-		-- vim.o.guicursor = table.concat({
-		-- 	-- "n:block-CursorNormal",
-		-- 	"i-ci:block-CursorInsert",
-		-- 	"v:block-CursorVisual",
-		-- 	-- "r-cr:block-CursorReplace",
-		-- }, ",")
-	end,
+			vim.keymap.set("n", "<leader>t", ":Themery<CR>")
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		-- lazy = false,
+		config = function()
+			require("gruvbox").setup({
+				underline = false,
+				italic = {
+					strings = false,
+					emphasis = false,
+					comments = false,
+					operators = false,
+					folds = false,
+				},
+				invert_selection = true,
+				invert_intend_guides = false,
+				contrast = "hard", -- can be "hard", "soft" or empty string
+			})
+			-- vim.cmd("colorscheme gruvbox")
+
+			-- local colors = require("gruvbox").palette
+			--
+			-- vim.o.termguicolors = true
+			--
+			-- -- vim.api.nvim_set_hl(0, "CursorNormal", { fg = normal.bg, bg = normal.fg })
+			-- vim.api.nvim_set_hl(0, "CursorInsert", { fg = colors.dark0_hard, bg = colors.bright_blue })
+			-- vim.api.nvim_set_hl(0, "CursorVisual", { fg = "#ffffff", bg = "#000000" })
+			-- -- vim.api.nvim_set_hl(0, "CursorReplace", { fg = normal.bg, bg = yellow.fg })
+			--
+			-- vim.o.guicursor = table.concat({
+			-- 	-- "n:block-CursorNormal",
+			-- 	"i-ci:block-CursorInsert",
+			-- 	"v:block-CursorVisual",
+			-- 	-- "r-cr:block-CursorReplace",
+			-- }, ",")
+		end,
+	},
+	{ "sainnhe/gruvbox-material" },
+	{ "rebelot/kanagawa.nvim" },
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "ayu-theme/ayu-vim" },
 }
