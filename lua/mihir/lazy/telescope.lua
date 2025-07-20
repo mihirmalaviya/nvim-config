0ef2652305532e29f73276d94164b38e8c1f247f
MihirMalaviya <mihirmalaviya1@gmail.com>
2025-07-19 22:45:59 -0400

    changed some bindings

--- lua/mihir/lazy/telescope.lua
+++ lua/mihir/lazy/telescope.lua
@@ -14,19 +14,24 @@ return {
 
     local builtin = require("telescope.builtin")
 
-    vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+    vim.keymap.set("n", "<leader>pf", function()
+      builtin.find_files({ cwd = vim.fn.getcwd() })
+    end)
+
     vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+
     vim.keymap.set("n", "<leader>ps", function()
       builtin.grep_string({ search = vim.fn.input("Grep > ") })
     end)
+
     vim.keymap.set("n", "<leader>pws", function()
-      local word = vim.fn.expand("<cword>")
-      builtin.grep_string({ search = word })
+      builtin.grep_string({ search = vim.fn.expand("<cword>") })
     end)
+
     vim.keymap.set("n", "<leader>pWs", function()
-      local word = vim.fn.expand("<cWORD>")
-      builtin.grep_string({ search = word })
+      builtin.grep_string({ search = vim.fn.expand("<cWORD>") })
     end)
+
     vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
   end,
 }
