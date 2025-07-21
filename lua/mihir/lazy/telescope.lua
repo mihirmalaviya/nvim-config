b3472928400e25136559f0f72a128825f4607e26
MihirMalaviya <mihirmalaviya1@gmail.com>
2025-07-21 07:58:56 -0400

    formatting change

--- lua/mihir/lazy/telescope.lua
+++ lua/mihir/lazy/telescope.lua
@@ -15,9 +15,7 @@ return {
     local builtin = require("telescope.builtin")
 
     vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-
     vim.keymap.set("n", "<leader>pr", builtin.oldfiles, {})
-
     vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 
     vim.keymap.set("n", "<leader>ps", function()
