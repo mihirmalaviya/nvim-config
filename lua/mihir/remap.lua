3711f5f6e3b95c3fd4993888aeaa3bda04465a90
MihirMalaviya <mihirmalaviya1@gmail.com>
2025-08-07 02:56:15 -0400

    messed with keybinds

--- lua/mihir/remap.lua
+++ lua/mihir/remap.lua
@@ -1,48 +1,49 @@
 vim.g.mapleader = " "
 
-vim.keymap.set({ "n", "v" }, "<Up>", "<Nop>")
-vim.keymap.set({ "n", "v" }, "<Down>", "<Nop>")
-vim.keymap.set({ "n", "v" }, "<Left>", "<Nop>")
-vim.keymap.set({ "n", "v" }, "<Right>", "<Nop>")
+local map = vim.keymap.set
 
-vim.keymap.set({ "n", "v" }, ";", ":", { noremap = true })
+map({ "n", "v" }, "<Up>", "<Nop>")
+map({ "n", "v" }, "<Down>", "<Nop>")
+map({ "n", "v" }, "<Left>", "<Nop>")
+map({ "n", "v" }, "<Right>", "<Nop>")
 
-vim.keymap.set("n", "<leader>q", ":q<CR>")
-vim.keymap.set("n", "<leader>w", ":w<CR>")
+map({ "n", "v" }, ";", ":", { noremap = true })
 
-vim.keymap.set("v", "<leader>x", ":lua<CR>")
+map("n", "<leader>o", ":update<CR> :source<CR>")
+map("n", "<leader>w", ":write<CR>")
+map("n", "<leader>q", ":quit<CR>")
+
+map({ "n", "v", "x" }, "<leader>s", ":e #<CR>")
+map({ "n", "v", "x" }, "<leader>S", ":sf #<CR>")
+
+map("v", "<leader>x", ":lua<CR>")
 
 -- center screen for some cursor stuff
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+map("n", "J", "mzJ`z")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
 
 -- moving text
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
-vim.keymap.set(
+map(
   "n",
   "<leader>T",
   function() vim.o.background = (vim.o.background == "dark") and "light" or "dark" end,
   { desc = "Toggle dark/light mode" }
 )
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
+map("x", "<leader>p", [["_dP]])
 
-vim.keymap.set(
-  "n",
-  "<leader>s",
-  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-  { desc = "Replace all instances of word" }
-)
-vim.keymap.set(
-  "v",
-  "<leader>s",
-  [["hy:%s/\V<C-r>h/<C-r>h/gI<Left><Left><Left>]],
-  { desc = "Replace all instances of selection" }
-)
+-- map(
+--   "n",
+--   "<leader>s",
+--   [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+--   { desc = "Replace all instances of word" }
+-- )
+-- map("v", "<leader>s", [["hy:%s/\V<C-r>h/<C-r>h/gI<Left><Left><Left>]], { desc = "Replace all instances of selection" })
