ebfe62dd0a9c8f703e385cbb1c80d029f5e1656b
MihirMalaviya <mihirmalaviya1@gmail.com>
2025-08-12 03:57:42 -0400

    changed theme to habamax and added transparent bg

--- lua/mihir/init.lua
+++ lua/mihir/init.lua
@@ -3,4 +3,27 @@ require("mihir.remap")
 require("mihir.autocmds")
 require("mihir.lazy")
 
-vim.cmd("colorscheme gruvbox-material-contrast")
+vim.cmd("colorscheme habamax")
+
+-- vim.cmd(":hi statusline guibg=NONE")
+
+local names = {
+  "Normal",
+  "NormalNC",
+  "EndOfBuffer",
+  "MsgArea",
+  "MsgSeparator",
+  "VertSplit",
+  "WinSeparator",
+  -- "StatusLine",
+}
+
+for _, n in pairs(names) do
+  local ok, gr = pcall(vim.api.nvim_get_hl, 0, { name = n })
+  if ok then
+    gr.bg = nil
+    gr.ctermbg = nil
+    gr.blend = 0
+    vim.api.nvim_set_hl(0, n, gr)
+  end
+end
